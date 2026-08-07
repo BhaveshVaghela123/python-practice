f5337affd83269cb621beb99e0f97306aab63ffc
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-08-07 17:31:56 +0530

    Add files via upload

---
+++ Find The Area of triangle.py
@@ -0,0 +1,5 @@
+#Find The Area of triangle
+Base = float(input("Enter the base of the triangle:"))
+Height = float(input("Enter the height of the triangle:"))
+Area = 0.5 * Base * Height
+print("The area of the triangle is:",Area)
