868356acbfa81810a27f0aa7b710cb3ddddf5dcd
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-08-09 15:03:57 +0530

    Add files via upload

---
+++ Input marks of three subjects of a student and print avg marks.py
@@ -0,0 +1,8 @@
+# input marks of three subjects of a student and calculate the average marks and print the result.
+subject1 = float(input("Enter marks for subject 1: "))
+subject2 = float(input("Enter marks for subject 2: "))
+subject3 = float(input("Enter marks for subject 3: "))
+
+average = (subject1 + subject2 + subject3) / 3
+
+print("The average marks is:", average)
