786a46d3c109e3eb67f2dc206b0d64acae6554d0
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-08-10 20:56:12 +0530

    Add files via upload

---
+++ Find the sum of natural numbers.py
@@ -0,0 +1,4 @@
+#find the sum of natural numbers
+numbers = int(input("Enter the number:"))
+sum_numbers = numbers * (numbers+1) // 2
+print("Sum of first", numbers, "natural numbers is:", sum_numbers)
